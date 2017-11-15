9576f7e5f93047cc8b15ba527ca707880a0cdc0e
lariac <lariac24@gmail.com>
2017-11-14 19:18:57 -0500

    Creación proyecto y configuración de Firebase
    Se creó el proyecto AprendeAComer en React Native y posteriormente se
    realizó la configuración de la base de datos Firebase con la aplicación.

---
+++ AprendeAComer/App.js
@@ -0,0 +1,69 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import * as firebase from 'firebase';
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyB18yHs-UzsY7ucPdvn-6Tq8YfNWZPkjMM",
+  authDomain: "aprendeacomer-78039.firebaseio.com",
+  databaseURL: "https://aprendeacomer-78039.firebaseio.com",
+  storageBucket: "aprendeacomer-78039.appspot.com",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
